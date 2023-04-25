86cd76669c07c5e0ca7f8ed352bd316265a725ea
Will Daughtridge <wdaughtridge@gmail.com>
2023-04-25 19:01:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
 
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   output: 'export'
 }
 
