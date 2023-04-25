b650b7c60b164dfeeab23063f774e057769251a3
Will Daughtridge <wdaughtridge@gmail.com>
2023-04-25 01:06:37 -0400

    config 2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = '';
-let basePath = '/';
+let assetPrefix = ''
+let basePath = '/'
 
 if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
 const nextConfig = {
